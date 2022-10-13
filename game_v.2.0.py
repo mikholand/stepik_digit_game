acf4afe1a571163c0c2a18563a3e92dcece871d7
mikholand <mikholand@gmail.com>
2022-10-13 10:50:55 +0200

    digit_game v.2.1 Добавлен подсчет сделанных попыток

--- game_v.2.0.py
+++ game_v.2.0.py
@@ -1,5 +1,7 @@
 from random import *
 
+count = 0
+
 # Функция проверки начала диапазона (является ли введенные данные числом)
 def range_begin():
     x = input('Введите начало диапазона: ')
@@ -9,14 +11,16 @@ def range_begin():
         start()
     return False
 
+# Функция проверки конца диапазона (является ли введенные данные числом)
 def range_end():
     y = input('Введите конец диапазона: ')
     if y.isdigit():
         return int(y)
-    if input('Вы ввели некоректное число. Хотели начать заново? Введите да/нет ').lower() in ['да', 'lf']:
+    if input('Вы ввели некоректное число. Хотите начать заново? Введите да/нет ').lower() in ['да', 'lf']:
         start()
     return False
 
+# Функция проверки корректности числа
 def is_valid(number, x, y):
     if str(number).isdigit() and x <= int(number) <= y:
         return True
@@ -34,7 +38,7 @@ def hello():
           'Например, от 1 до 10 или от 128 до 256')
     input('Для дальнейшей инструкции нажми Enter')
 
-# Функция
+# Функция диапазона игры
 def range_game():
     x = range_begin()
     y = range_end()
@@ -46,8 +50,7 @@ def range_game():
         x, y = y, x
     return x, y
 
-
-# Функция ввода числа
+# Функция запуска игры
 def start():
     print()
     x, y = range_game()
@@ -55,20 +58,34 @@ def start():
     random_digit = randint(x, y)
     game(random_digit, x, y)
 
+# Функция перезапуска игры
 def restart():
+    global count
     if input('Хотели начать заново? Введите да/нет ').lower() in ['да', 'lf']:
+        count = 0
         start()
     else:
         print('Спасибо за игру!')
 
+# Функция вывода количества попыток
+def print_count(count):
+    if count == 1:
+        print('Вау! А вы везунчик! Отгадали его с 1 попытки')
+    elif count == 2:
+        print('Весьма неплохо! Отгадали его со 2 попытки')
+    elif count >= 3:
+        print(f'Отгадали его с {count} попытки')
 
 # Функция алгоритма игры
 def game(random_digit, x, y):
+    global count
     digit = input(f'Введите число от {x} до {y}: ')
+    count += 1
     if is_valid(digit, x, y):
         digit = int(digit)
         if digit == random_digit:
             print('Вы угадали число! Поздравляю!')
+            print_count(count)
             restart()
         elif digit < random_digit:
             print('Ваше число оказалось меньше загаданного. Попробуй еще раз.')
